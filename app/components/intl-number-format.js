6f96d63cc3a4b523f7f8092a1ac37c632b78cb9d
Randall Morey <randall@randallmorey.com>
2019-01-20 16:25:11 -0500

    add number format component

---
+++ app/components/intl-number-format.js
@@ -0,0 +1,14 @@
+import Component from '@ember/component';
+import { computed } from '@ember/object';
+
+export default Component.extend({
+  locale: 'en-US',
+
+  numberValue: 12345678.56789,
+
+  formattedNumber: computed('numberValue', 'locale', function () {
+    const locale = this.locale;
+    const formatter = new Intl.NumberFormat(locale);
+    return formatter.format(this.numberValue);
+  })
+});
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,14 +3,6 @@ import { computed } from '@ember/object';
 
 export default Controller.extend({
 
-  locale: 'en-US',
-
-  numberValue: 12345678.56789,
-
-  formattedNumber: computed('numberValue', 'locale', function () {
-    const locale = this.locale;
-    const formatter = new Intl.NumberFormat(locale);
-    return formatter.format(this.numberValue);
-  })
+  locale: 'en-US'
 
 });
